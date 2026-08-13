d3f4a29434cf67f30d38c3f49254ac23ea0cfef7
Thomas Adamson <taadamson33@gmail.com>
2026-08-13 14:27:24 -0400

    finished spi_master_tb, next will implement top_tb to instantiate tests for all SPI modes.

--- basys3_SPI.srcs/sim_1/new/spi_master_tb.sv
+++ basys3_SPI.srcs/sim_1/new/spi_master_tb.sv
@@ -13,11 +13,10 @@ module spi_master_tb #(
 );
 
     localparam int SLAVE_SEL_WIDTH = (NUM_SLAVES <= 1) ? 1 : $clog2(NUM_SLAVES);
-    localparam int CYCLES_SPI = CLK_FREQ / SPI_FREQ;
-    localparam int HALF_CYCLES = CYCLES_SPI/2;
+    localparam real CLK_PERIOD = 1_000_000_000.0/CLK_FREQ; // 1_000_000_000 ns/s / 100M cycles/s
 
     logic clk = 0;
-    always #5 clk = ~clk; //100 MHz
+    always #(CLK_PERIOD/2) clk = ~clk; //100 MHz
     
     logic reset;
     
@@ -62,15 +61,15 @@ spi_master #(
     endtask
     
     
-    task automatic wait_leading_edge(input int polarity);
-        if(polarity)
+    task automatic wait_leading_edge;
+        if(CPOL)
             @(negedge sclk);
         else
             @(posedge sclk);
     endtask
     
-    task automatic wait_trailing_edge(input int polarity);
-        if(polarity)
+    task automatic wait_trailing_edge;
+        if(CPOL)
                 @(posedge sclk);
             else
                 @(negedge sclk);
@@ -78,53 +77,80 @@ spi_master #(
     
     task automatic send_and_check(input [DATA_WIDTH-1:0] data, input [SLAVE_SEL_WIDTH-1:0] slave_idx);
     
+        logic [DATA_WIDTH-1:0] miso_data;
+        logic [DATA_WIDTH-1:0] expected_miso_data;
+        logic [DATA_WIDTH-1:0] mosi_data; 
         
-        logic [DATA_WIDTH-1:0] miso_data = '0;
-        logic [DATA_WIDTH-1:0] mosi_data = '0;
+        mosi_data = '0;
+        
+        if(data == '0) begin
+            //respond to request with fake data
+            miso_data = $urandom;
+        end else begin
+            //respond with dummy data when master is sending request
+            miso_data = '0;
+        end
+        
+        expected_miso_data = miso_data;
+        
+        
+        
+        @(negedge clk);
+        tx_start = 1'b1;
         slave_select = slave_idx;
         tx_data = data;
         
-        tx_start = 1'b1;
-        wait_clocks(1);
+        @(negedge clk);
         tx_start = 1'b0;
         
         //wait 1 clk cycle for IDLE >> ASSERT_CS_N state transition
         wait_clocks(1);
         //check busy flag
         assert(busy == 1'b1)
-            else $error("busy not asserted");
+        else begin
+            $error("busy not asserted");
+            test_pass = 0;
+        end
+        
+        assert(cs_n[slave_select] == 0 && $countones(cs_n) == NUM_SLAVES-1)
+        else begin
+            $error("incorrect slave(s) selected");
+            test_pass = 0;
+        end
             
             
        //wait 1 clk cycle for ASSERT_CS_N >> DATA state transition     
        wait_clocks(1);
        
        assert(sclk == CPOL)
-            else $error("sclk polarity incorrect");
+       else begin
+            $error("sclk polarity incorrect");
+            test_pass = 0;
+       end
        
+       if(!CPHA) begin
+            // preload first MISO bit
+            miso = miso_data[DATA_WIDTH-1];
+            miso_data = miso_data << 1;
+       end
        for(int i = 0; i<DATA_WIDTH; i++) begin
             if(CPHA) begin
                 //leading edge shift (MISO)
-                wait_leading_edge(CPOL);
+                wait_leading_edge;
                 miso = miso_data[DATA_WIDTH-1];
                 miso_data = miso_data << 1;
                 
                 //trailing edge sample (MOSI)
-                wait_trailing_edge(CPOL);
+                wait_trailing_edge;
                 mosi_data = {mosi_data[DATA_WIDTH-2:0], mosi};
                 
             end else begin
                 //leading edge sample (MOSI)
-                if(i == 0) begin
-                    miso = miso_data[DATA_WIDTH-1];
-                    miso_data = miso_data << 1;
-                    wait_leading_edge(CPOL);
-                    mosi_data = {mosi_data[DATA_WIDTH-2:0], mosi};
-                end else begin
-                    wait_leading_edge(CPOL);
-                    mosi_data = {mosi_data[DATA_WIDTH-2:0], mosi};
-                end
+                wait_leading_edge;
+                mosi_data = {mosi_data[DATA_WIDTH-2:0], mosi};
+                
                 //trailing edge shift MISO)
-                wait_trailing_edge(CPOL);
+                wait_trailing_edge;
                 if(i == DATA_WIDTH-1) begin
                     //do nothing
                 end else begin
@@ -136,12 +162,51 @@ spi_master #(
        end
        
        
+       //DEASSERT_CS_N state
+       
+       @ (posedge done);
+       
+       assert(sclk == CPOL)
+       else begin
+            $error("sclk did not converge to CPOL");
+            test_pass = 0;
+        end
+            
+       assert(busy == 1'b0)
+       else begin
+            $error("busy flag did not clear");
+            test_pass = 0;
+        end
+            
+       assert(rx_data == expected_miso_data)
+       else begin
+            $error("miso data mismatch");
+            test_pass = 0;
+        end
+            
+        assert(data == mosi_data)
+        else begin
+            $error("mosi data mismatch");
+            test_pass = 0;
+        end
+            
+        //idle state return
+        @(posedge clk);
         
+        assert(done == 1'b0)
+        else begin
+            $error("done flag not cleared");
+            test_pass = 0;
+        end
         
+        assert(cs_n == '1)
+        else begin
+            $error("Slave select still active");
+            test_pass = 0;
+        end
         
-        
-        
-        
+        if(test_pass)
+            $display("PASS: TX=0x%0h RX=0x%0h slave=%0d", data, rx_data, slave_select);
         
     endtask
     
@@ -150,10 +215,19 @@ spi_master #(
     initial begin
     
         int random_slave;
-        //init
+        
+        test_pass = 1;
+        test_done = 0;
+        tx_start = 1'b0;
+        tx_data = '0;
+        slave_select = 0;
+        miso = 1'b0;
         reset = 1'b1;
         
-        repeat(5) @(posedge clk);
+        //init       
+        wait_clocks(2);
+        
+        @(negedge clk);
         reset = 1'b0;
         
         
@@ -167,7 +241,11 @@ spi_master #(
         send_and_check('h3C,random_slave);
         send_and_check('0,random_slave); //dummy bytes
         
-        $display("All tests done.");
-        $finish;
+        if(test_pass)
+            $display("CPOL=%0d CPHA=%0d PASS", CPOL, CPHA);
+        else
+            $display("CPOL=%0d CPHA=%0d FAILED", CPOL, CPHA);
+        
+        test_done = 1;
     end
 endmodule
