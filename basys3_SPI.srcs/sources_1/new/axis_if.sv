2f821497d6d113181256b7edb282d6850c45a3d9
Thomas Adamson <taadamson33@gmail.com>
2026-09-10 16:22:06 -0400

    Add AXI4-Stream interface with source and sink modports

---
+++ basys3_SPI.srcs/sources_1/new/axis_if.sv
@@ -0,0 +1,40 @@
+interface axis_if #(
+    parameter int DATA_WIDTH = 8,
+    parameter int NUM_DESTINATIONS = 1
+);
+
+    logic ACLK;
+    logic ARESETn;
+    
+    logic [DATA_WIDTH-1:0] TDATA;
+    logic TVALID;
+    logic TREADY;
+    logic TLAST;
+    logic [((NUM_DESTINATIONS <= 1) ? 1 : $clog2(NUM_DESTINATIONS))-1:0] TDEST;
+    
+    modport source (
+        output TDATA,
+        output TVALID,
+        output TLAST,
+        output TDEST,
+        
+        input TREADY,
+        
+        input ACLK,
+        input ARESETn
+    );
+    
+    modport sink (
+        output TREADY,
+        
+        input TDATA,
+        input TVALID,
+        input TLAST,
+        input TDEST,
+        
+        input ACLK,
+        input ARESETn
+    );
+    
+
+endinterface
